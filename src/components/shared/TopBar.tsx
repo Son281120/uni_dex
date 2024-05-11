d035c6251edba20f4ee1b07b8a9d3c9cb6b3d50b
Son281120 <changngoc10x28@gmail.com>
2024-05-11 19:48:22 +0700

    fea: navigation_bar

--- src/components/shared/TopBar.tsx
+++
@@ -1,9 +0,0 @@
-import ConnectButton from "./ConnectButton";
-import ConnectWallet from "./ConnectWallet";
-
-export default function TopBar () {
-  return <div className="flex items-center justify-between px-3 py-5">
-    <h1>Topbar</h1>
-    <ConnectButton/>
-  </div>
-}
